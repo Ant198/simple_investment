ad692ebf2df9ac4ccf7270f7b23108b9fc6ee2dc
Ant198 <ridnovir@yahoo.com>
2024-11-20 19:07:32 +0200

    edit provider

--- simple_investment/lib/provider/company_data_provider.dart
+++ simple_investment/lib/provider/company_data_provider.dart
@@ -4,24 +4,63 @@ import 'dart:convert' as convert;
 
 class CompanyDataProvider extends ChangeNotifier {
   late Map<String, dynamic> _ePSStore = {};
+  late Map<String, dynamic> _incomeStatement = {};
+  late Map<String, dynamic> _balanceSheet = {};
+  late Map<String, dynamic> _cashFlow = {};
+
   Map<String, dynamic> get ePSStore => _ePSStore;
-  set ePSStore(Map<String, dynamic> value) {
-    _ePSStore = value;
-    notifyListeners();
-  }
+  Map<String, dynamic> get incomeStateent => _incomeStatement;
+  Map<String, dynamic> get balanceSheet => _balanceSheet;
+  Map<String, dynamic> get cashFlow => _cashFlow;
+
   Future<void> fetchEarnings(String ticker) async {
-  String apiKey = 'XSEM76YOZ2K24533';
-  String symbol = ticker;
-  String url = 'https://www.alphavantage.co/query?function=EARNINGS&symbol=$symbol&apikey=$apiKey';
-  final response = await http.get(Uri.parse(url));
-  if (response.statusCode == 200) {
-    final jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
-    _ePSStore = jsonResponse;
-    notifyListeners();
+    String apiKey = 'XSEM76YOZ2K24533';
+    String symbol = ticker;
+    String url = 'https://www.alphavantage.co/query?function=EARNINGS&symbol=$symbol&apikey=$apiKey';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      _ePSStore = jsonResponse;
+      notifyListeners();
+    }
   }
 
+  Future<void> fetchIncomeStatement(String ticker) async {
+    String apiKey = 'XSEM76YOZ2K24533';
+    String symbol = ticker;
+    String url = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=$symbol&apikey=$apiKey';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      _incomeStatement = jsonResponse;
+      notifyListeners();
+    }
+  }
+
+  Future<void> fetchBalanceSheet(String ticker) async {
+    String apiKey = 'XSEM76YOZ2K24533';
+    String symbol = ticker;
+    String url = 'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=$symbol&apikey=$apiKey';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      _balanceSheet = jsonResponse;
+      notifyListeners();
+    }
+  }
+
+  Future<void> fetchCashFlow(String ticker) async {
+    String apiKey = 'XSEM76YOZ2K24533';
+    String symbol = ticker;
+    String url = 'https://www.alphavantage.co/query?function=CASH_FLOW&symbol=$symbol&apikey=$apiKey';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      _cashFlow = jsonResponse;
+      notifyListeners();
+    }
+  }
 }
   /*
    fcf = operatingCashFlow - caEx
   */
-}
